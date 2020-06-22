2f79d2007b692a190ef2bf3a7a7afc1ba6ac7c13
NakaDaoLe <genesis@nakadaole.com>
2020-06-22 16:34:02 +0800

    fix

--- src/components/SingleName/SubDomains.js
+++ src/components/SingleName/SubDomains.js
@@ -116,7 +116,7 @@ function SubDomains({
           }}
         >
           {({ loading, error, data, refetch }) => {
-            if (error || !data.domain) {
+            if (error) {
               console.error(
                 'Unable to get subdomains from subgraph, falling back to web3 ',
                 error
